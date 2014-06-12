e6731f1c1451fce45b94c822d3eb1a0c0545be2f
Peter Hollingsworth <peter@hollingsworth.net>
2014-06-11 17:31:22 -0700

    updated version

--- rest_framework_drilldown/__init__.py
+++ rest_framework_drilldown/__init__.py
@@ -1,4 +1,4 @@
 from .views import DrillDownAPIView
 
-VERSION = (0, 1, 0)
+VERSION = (0, 1, 1)
 __version__ = VERSION # alias
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='djangorestframework-drilldown',
-    version='0.1.0',
+    version='0.1.1',
     url='http://github.com/peterh32/django-rest-framework-drilldown',
     license='MIT',
     packages=['rest_framework_drilldown'],
